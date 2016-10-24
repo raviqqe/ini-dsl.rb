d72f0e834b47a1355f0d4248fa1d5ce9ecf894a7
Yota Toyama <raviqqe@gmail.com>
2016-10-24 23:38:28 +0900

    Fix examples and test

--- examples/complex.rb
+++ examples/complex.rb
@@ -4,8 +4,6 @@ require 'ini-dsl'
 
 
 conf = ini_dsl do
-  user :nobody, :nogroup
-  worker_processes 1
   events do
     worker_connections 1024
   end
@@ -16,31 +14,21 @@ conf = ini_dsl do
 
     sendfile :on
     keepalive_timeout 65
+  end
 
-    server do
-      listen 80
-      server_name :localhost
+  server do
+    listen 443
+    server_name :localhost
 
-      location '/' do
-        root '/usr/local/www/nginx'
-        index 'index.html', 'index.htm'
-      end
-    end
+    ssl_certificate 'cert.pem'
+    ssl_certificate_key 'key.pem'
+    ssl_session_cache 'shared:SSL:1m'
+    ssl_session_timeout '5m'
+  end
 
-    server do
-      listen 443, :ssl
-      server_name :localhost
-
-      ssl_certificate 'cert.pem'
-      ssl_certificate_key 'key.pem'
-      ssl_session_cache 'shared:SSL:1m'
-      ssl_session_timeout '5m'
-
-      location '/' do
-        root 'html'
-        index 'index.html', 'index.htm'
-      end
-    end
+  location do
+    root 'html'
+    index 'index.html'
   end
 end
 
